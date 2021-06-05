5723af7978e7473772aa740100ebcac23b9971de
daffahilmyf <daffahilmyf@student.telkomuniversity.ac.id>
2021-06-05 13:04:24 +0700

    Create admin class

--- TUBES_KPL/Model/Person/Admin.cs
+++ TUBES_KPL/Model/Person/Admin.cs
@@ -1,12 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Newtonsoft.Json;
+using System;
 
 namespace Tubes_KPL.Model.Person
 {
     public class Admin: Employee
     {
+        private string _AdminNIK;
 
+        public Admin() { }
+
+        public Admin(string employeeName, string employeeAddress) : base(employeeName, employeeAddress)
+        {
+
+            EmployeeLevelAccess = 1;
+        }
+        public Admin(string employeeName, string employeeAddress, string adminNIK) : base(employeeName, employeeAddress)
+        {
+
+            AdminNIK = adminNIK;
+            EmployeeLevelAccess = 1;
+        }
+
+
+        [JsonProperty(PropertyName = "NIK", Order = 5)]
+        public string AdminNIK
+        {
+            get { return _AdminNIK; }
+            set
+            {
+                try
+                {
+                    if (String.IsNullOrEmpty(value)) 
+                        throw new ArgumentNullException("NIK should not be blank!");
+
+                    _AdminNIK = value;
+                }
+                catch (ArgumentNullException e)
+                {
+                    Console.Error.WriteLine(e.ParamName);
+                }
+            }
+        }
 
     }
 }
